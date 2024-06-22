a59a4c15c2b87b3f53f62fbd3398a44763a3bca8
JiaweiMao <jiaweimao2019@outlook.com>
2024-06-23 07:20:21 +0800

    add test

---
+++ src/main/java/mjw/hipparchus/BetaDistributionTest.java
@@ -0,0 +1,14 @@
+package mjw.hipparchus;
+
+import org.junit.jupiter.api.Test;
+
+public class BetaDistributionTest {
+    @Test
+    void test() {
+        int[] trails = new int[]{0, 1, 2, 3, 4, 8, 16, 32, 50, 150};
+        int[] heads = new int[]{0, 1, 1, 1, 1, 4, 6, 9, 13, 48};
+        int[][] betaParams = new int[][]{{1, 1}, {20, 20}, {1, 4}};
+
+
+    }
+}
